2da7c51cbc961c538dd1a9967506ba51b19ac818
Alex <AlexDankoXXL@gmail.com>
2021-08-31 15:24:25 +0300

    HomeTest

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 ﻿#include <iostream>
-
+#include <Windows.h>
 #include "Builder.h"
 
 using namespace std;
 
 int main()
 {
-
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(0, "");
+	srand(time(0));
+	
 	Director* director = new Director;
 	MusicCenterBuilder* mcBuilder = new MusicCenterBuilder;
 	director->setBuilder(mcBuilder);
