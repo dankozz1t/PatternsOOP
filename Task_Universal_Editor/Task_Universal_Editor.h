06a9a1858b4fa06f95c3829a6e6c19a268937491
Alex <AlexDankoXXL@gmail.com>
2021-09-09 19:20:50 +0300

    Done Task Uni. Editor

--- Task_Universal_Editor/Task_Universal_Editor.h
+++ Task_Universal_Editor/Task_Universal_Editor.h
@@ -14,7 +14,7 @@ protected:
 public:
 	TextFile(string n) :name(n) {}
 
-	string setName(string nam) { name = nam; }
+	void setName(string nam) { name = nam; }
 	string getName() { return name; }
 
 	void work()
@@ -27,11 +27,12 @@ public:
 			cout << " 2 - перевести на другу строку" << endl;
 			cout << " 3 - закрыть" << endl;
 
-			cout << "Введите: "; cin >> menu;
+			cout << "Введите меню: "; cin >> menu;
 			switch (menu)
 			{
 			case 1: write(); break;
 			case 2: text += "\n"; break;
+			default: break;
 			}
 		}
 	}
@@ -65,6 +66,9 @@ public:
 	virtual void saveNewName() = 0;
 	virtual void close() = 0;
 	virtual void print() = 0;
+	virtual void deleteFile() = 0;
+
+	virtual void getMenu(Editor*) = 0;
 
 };
 
@@ -89,7 +93,7 @@ public:
 		cout << "Существующие файлы: ";
 		for (auto& c : files)
 			cout << c->getName() << ", ";
-		
+
 		cout << "\n\nВведите имя файла: ";
 		string nameFile; cin >> nameFile;
 
@@ -99,7 +103,7 @@ public:
 			{
 				cout << " * Вы открыли файл: " << files[i]->getName() << endl;
 				acviteFiles = i;
-				
+
 				files[acviteFiles]->work();
 			}
 		}
@@ -107,25 +111,78 @@ public:
 
 	void save() override
 	{
+		cout << " *-- " << files[acviteFiles]->getName() << " --*" << endl;
 		files[acviteFiles]->print();
 		cout << " * СОХРАНЕНО!!!! * " << endl;
 	}
 
 	void saveNewName() override
 	{
-		cout << " * Сохранил под новым именем" << endl;
+		cout << " *-- " << files[acviteFiles]->getName() << " --*" << endl;
+		
+		cout << "Введите новое имя: "; string newName; cin >> newName;
+		
+		files[acviteFiles]->setName(newName);
+		cout << " * Фаил сохранен под новым именем: * " << files[acviteFiles]->getName() << endl;
 	}
 
 	void close() override
 	{
-		cout << " * Закрыл" << endl;
+		if (acviteFiles == 0) //Если Активный файл(окно) закрыть, откроется предыдущий
+			cout << " * Нет активных окон или же оно одно." << endl;
+		else
+		{
+			cout << "Активный файл " << files[acviteFiles]->getName() << " закрыт." << endl;
+			acviteFiles--;
+			cout << "Теперь Активный файл " << files[acviteFiles]->getName() << endl;
+		}
 	}
-	
+
 	void print() override
 	{
+		cout << " *-- " << files[acviteFiles]->getName() << " --*" << endl;
 		files[acviteFiles]->print();
 	}
 
+	void deleteFile() override
+	{
+		cout << "Активный файл " << files[acviteFiles]->getName() << " удален." << endl;
+		files.erase(files.begin() + acviteFiles);
+		cout << "Теперь Активный файл " << files[acviteFiles]->getName() << endl;
+	}
+
+
+	void getMenu(Editor* editor) override
+	{
+		int menu = 0;
+		do
+		{
+			system("cls");
+			cout << " *-- ТЕКТОВЫЙ РЕДАКТОР -- *" << endl;
+			cout << " * 1 - Создать " << endl;
+			cout << " * 2 - Открыть " << endl;
+			cout << " * 3 - Сохранить " << endl;
+			cout << " * 4 - Сохранить под новым именем " << endl;
+			cout << " * 5 - Показать " << endl;
+			cout << " * 6 - Закрыть " << endl;
+			cout << " * 7 - Удалить " << endl;
+			cout << " * 8 - Выход " << endl;
+
+			cout << "Введите: "; cin >> menu;
+
+			switch (menu)
+			{
+			case 1: system("cls"); editor->create(); break;
+			case 2: system("cls"); editor->open(); break;
+			case 3: system("cls"); editor->save(); system("pause"); break;
+			case 4: system("cls"); editor->saveNewName(); system("pause"); break;
+			case 5: system("cls"); editor->print(); system("pause"); break;
+			case 6: system("cls"); editor->close(); system("pause"); break;
+			case 7: system("cls"); editor->deleteFile(); system("pause");  break;
+			}
+
+		} while (menu != 8);
+	}
 };
 
 
@@ -156,10 +213,55 @@ public:
 	{
 		cout << " * Закрыл" << endl;
 	}
-	
+
 	void print() override
 	{
-		cout << " * Посмотрел" << endl;
+		cout << " * Посмотрел/Печать" << endl;
+	}
+
+	void deleteFile() override
+	{
+		cout << " * Удалил" << endl;
+
+	}
+
+	void palette()
+	{
+		cout << " * Использую палитру!" << endl;
+	}
+
+	void getMenu(Editor* editor) override
+	{
+		int menu = 0;
+		do
+		{
+			system("cls");
+			cout << " *-- ГРАФИЧЕСКИЙ РЕДАКТОР -- *" << endl;
+			cout << " * 1 - Создать " << endl;
+			cout << " * 2 - Открыть " << endl;
+			cout << " * 3 - Сохранить " << endl;
+			cout << " * 4 - Сохранить под новым именем " << endl;
+			cout << " * 5 - Показать/Печать " << endl;
+			cout << " * 6 - Закрыть " << endl;
+			cout << " * 7 - Удалить " << endl;
+			cout << " * 8 - ПАЛИТРА " << endl;			
+			cout << " * 9 - Выход " << endl;
+
+			cout << "Введите: "; cin >> menu;
+
+			switch (menu)
+			{
+			case 1: system("cls"); editor->create(); system("pause"); break;
+			case 2: system("cls"); editor->open(); system("pause"); break;
+			case 3: system("cls"); editor->save(); system("pause"); break;
+			case 4: system("cls"); editor->saveNewName(); system("pause"); break;
+			case 5: system("cls"); editor->print(); system("pause"); break;
+			case 6: system("cls"); editor->close(); system("pause"); break;
+			case 7: system("cls"); editor->deleteFile(); system("pause");  break;
+			case 8: system("cls"); GraphicsEditor::palette(); system("pause");  break; //TODO: ?????
+			}
+
+		} while (menu != 9);
 	}
 };
 
@@ -180,7 +282,6 @@ class TextEditorCreator : public Creator
 public:
 	Editor* createEditor() const override
 	{
-		cout << " * Вы создали текстовый редактор" << endl;
 		return new TextEditor;
 	}
 };
@@ -190,43 +291,42 @@ class GraphicsEditorCreator : public Creator
 public:
 	Editor* createEditor() const override
 	{
-		cout << " * Вы создали текстовый редактор" << endl;
 		return new GraphicsEditor;
 	}
 };
 
 int mainUniversal_Editor()
 {
-	Creator* creator = new TextEditorCreator;
-	Editor* editor;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(0, "");
+	SetConsoleTitleA("Universal Editor");
+	
+	Creator* creator = nullptr;
+	Editor* editor = nullptr;
 
-	editor = creator->create();
 	int menu = 0;
-	do
+	while (menu != 3)
 	{
 		system("cls");
-		cout << " * 1 - Создать " << endl;
-		cout << " * 2 - Открыть " << endl;
-		cout << "   3 - Сохранить " << endl;
-		cout << "   4 - Сохранить под новым именем " << endl;
-		cout << " * 5 - Показать " << endl;
-		cout << "   6 - Закрыть " << endl;
-		cout << " * 7 - Выход " << endl;
+		cout << "ВЫ ОТКРЫЛИ РЕДАКТОР. КАКОЙ РЕДАКТОР ВАС ИНТЕРЕСУЕТ?" << endl;
+		cout << " 1 - Открыть Текстовый редактор." << endl;
+		cout << " 2 - Открыть Графический редактор." << endl;
+		cout << " 3 - Выход." << endl;
 
-		cout << "Введите: "; cin >> menu;
-		//TODO: другое меню для Графического (Палитра)
+		cout << "Введите: ";  cin >> menu;
 
 		switch (menu)
 		{
-		case 1: system("cls"); editor->create(); break;
-		case 2: system("cls"); editor->open(); break;
-		case 3:  break;
-		case 4:  break;
-		case 5: system("cls"); editor->print(); system("pause"); break;
-		case 6:  break;
+		case 1: creator = new TextEditorCreator; break;
+		case 2:	creator = new GraphicsEditorCreator; break;
+		case 3: return 0;
 		}
 
-	} while (menu != 7);
+		editor = creator->create();
+		editor->getMenu(editor);
+	}
 
 	return 0;
+
 }
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 
 #include "Patterns/ChainOfCommand.h"
+#include "Task_Universal_Editor/Task_Universal_Editor.h"
 
 using namespace std;
 
@@ -13,29 +14,27 @@ void goToRush()
 }
 
 
-int main()
-{
-	goToRush();
-
-	Client* client = new Client(new Economict(new Buhfalter(new Director(nullptr))));
-	client->sendPayment(300);
-	client->sendPayment(1500);
-	client->sendPayment(6000);
-	client->sendPayment(10000);
-
-	return 0;
-}
-
-
-
 //int main()
 //{
 //	goToRush();
-//	
-//	mainUniversal_Editor();
-//	
+//
+//	Client* client = new Client(new Economict(new Buhfalter(new Director(nullptr))));
+//	client->sendPayment(300);
+//	client->sendPayment(1500);
+//	client->sendPayment(6000);
+//	client->sendPayment(10000);
+//
 //	return 0;
 //}
+
+
+
+int main()
+{	
+	mainUniversal_Editor();
+	
+	return 0;
+}
 
 
 
